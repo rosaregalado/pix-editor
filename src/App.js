59ebb3939539bf111723af1eaf6790a926aa0678
Rosa Regalado <rosaregalado101@gmail.com>
2022-03-16 15:15:13 -0700

    structure of editor

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Slider from './Slider'
+import SidebarItem from './SidebarItem'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <div className='main-image' style={{ backgroundImage: "url(/images/sample_photo.jpeg)" }}></div>
+      <div className='sidebar'>
+        <SidebarItem />
+      </div> 
+      <Slider /> 
     </div>
   );
 }
